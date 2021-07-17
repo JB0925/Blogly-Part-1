0d9eb44487710a79cea2e2652126e033b214b407
Jesse Brink <jbrink0925@gmail.com>
2021-07-17 02:18:01 -0400

    finish MVP of app

--- app.py
+++ app.py
@@ -1,12 +1,90 @@
-"""Blogly application."""
+from flask import Flask, render_template, request, redirect, url_for
+from decouple import config
 
-from flask import Flask
-from models import db, connect_db
+from models import db, connect_db, User
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+app.config['SQLALCHEMY_DATABASE_URI'] = config('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
 db.create_all()
+
+
+@app.route('/')
+def home():
+    return redirect(url_for('user_list'))
+
+
+@app.route('/users')
+def user_list():
+    users = User.query.all()
+    if users:
+        users = [[f'{user.last_name}, {user.first_name}', user.id] for user in users]
+        return render_template('userlist.html', users=users)
+    
+    return render_template('userlist.html')
+
+
+@app.route('/users/new', methods=['GET', 'POST'])
+def add_user():
+    if request.method == 'GET':
+        return render_template('userform.html')
+    
+    form = request.form
+    first = form['first']
+    last = form['last']
+    image = form['image']
+
+    if image:
+        user = User(first_name=first, last_name=last, image_url=image)
+    else:
+        user = User(first_name=first, last_name=last)
+    
+    db.session.add(user)
+    db.session.commit()
+    return redirect(url_for('user_list'))
+
+
+@app.route('/users/<id>')
+def show_user(id):
+    user = User.query.get(id)
+    if user:
+        return render_template('userdisplay.html', user=user)
+    return redirect(url_for('user_list'))
+
+
+@app.route('/users/<id>/edit', methods=['GET', 'POST'])
+def edit_profile(id):
+    user = User.query.get(id)
+    categories = 'first_name last_name image_url'.split()
+    if user:
+        if request.method == 'GET':
+            return render_template('edit.html', id=user.id)
+    
+        form = request.form
+        for i in range(len(categories)):
+            if form[categories[i]]:
+                if i == 0:
+                    user.first_name = form[categories[i]]
+                elif i == 1:
+                    user.last_name = form[categories[i]]
+                else:
+                    user.image_url = form[categories[i]]
+
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('user_list', id=user.id))
+    else:
+        return redirect(url_for('user_list'))
+
+
+@app.route('/users/<id>/delete')
+def delete_user(id):
+    user = User.query.get(id)
+    if user:
+        db.session.delete(user)
+        db.session.commit()
+    
+    return redirect(url_for('user_list'))
--- models.py
+++ models.py
@@ -1,1 +1,22 @@
-"""Models for Blogly."""
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
+
+class User(db.Model):
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    image_url = db.Column(db.String, default='https://cdn5.vectorstock.com/i/thumb-large/66/14/default-avatar-photo-placeholder-profile-picture-vector-21806614.jpg')
+
+    
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}(id={self.id}, first={self.first_name}, last={self.last_name}, img={self.image_url}'
